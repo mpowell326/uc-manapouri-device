0d2ae1978646c22c6083ae0504e11e5fb681242e
Morgan Powell <map146@uclive.ac.nz>
2016-08-10 16:27:57 +1200

    Add motor module

--- src_main/main.c
+++ src_main/main.c
@@ -1,9 +1,24 @@
 /* Hello World program */
 
 #include <stdio.h>
+#include "Wiring.h"
+
+void setup(){
+	init_motors();
+
+}
+
 
 main(void)
 {
+	int pot_var = 0;
     printf("Hello World\n");
+    
+
+    while(1){
+    	pot_var = analogRead(adcPin)
+    }
+
+
     return 0;
 }
