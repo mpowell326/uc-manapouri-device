a308f02b46b102bed6018de4187813f519adcb6f
Morgan Powell <map146@uclive.ac.nz>
2016-08-25 11:10:51 +1200

    Test LED

--- src_main/main.c
+++ src_main/main.c
@@ -4,10 +4,19 @@
 #include "Wiring.h"
 
 
+
+void setup()
+{
+    pinMode(8_10, OUTPUT);
+}
+
+
+
 main(void)
 {
     pinMode(P8_10, OUTPUT);
     printf("Hello World !!\n");
 
+     digitalWrite(8_10, HIGH);
     return 0;
 }
