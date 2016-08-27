52628a2992a709ac6f50df9b73ac3533ae999423
mww38 <mww38@uclive.ac.nz>
2016-08-27 13:34:01 +1200

    update

--- src_main/motors.c
+++ src_main/motors.c
@@ -1,3 +1,23 @@
 #include "motors.h"
 
+#define freq_hz 50;
 
+void Motormove (int motorpin, int PWMpercentage, int time){
+
+
+
+    setTimePeriod (motorpin, uint32_t period_us);
+    setFrequency (motorpin, freq_hz);
+    setPulseWidth (motorpin, uint32_t period_us);
+    setDutyPercentage (motorpin, uint32_t percentage);
+
+
+
+    // length of time the motor is running for
+    delay(time)
+
+    // returning the motor to 1500hz
+    analogWrite(motorpin, uint8_t value)
+
+
+}
