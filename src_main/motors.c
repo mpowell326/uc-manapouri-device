b4e915fc634ac6949b2fdf5f2243c0304b6e676b
Morgan Powell <map146@uclive.ac.nz>
2016-09-09 13:07:48 +1200

    Testing IR - bugs

--- src_main/motors.c
+++ src_main/motors.c
@@ -2,20 +2,20 @@
 #include <Wire.h>
 #define freq_hz 50;
 
-void Motormove (int motorpin1, int motorpin2, int PWMpercentage, int time){
+// void Motormove (int motorpin1, int motorpin2, int PWMpercentage, int time){
 
 
-    //motor one
-    setTimePeriod (motorpin, uint32_t period_us);
-    setFrequency (motorpin, freq_hz);
-    setPulseWidth (motorpin, uint32_t period_us);
-    setDutyPercentage (motorpin, uint32_t percentage);
+//     //motor one
+//     setTimePeriod (motorpin, uint32_t period_us);
+//     setFrequency (motorpin, freq_hz);
+//     setPulseWidth (motorpin, uint32_t period_us);
+//     setDutyPercentage (motorpin, uint32_t percentage);
 
-    //motor two
-    setTimePeriod (motorpin, uint32_t period_us);
-    setFrequency (motorpin, freq_hz);
-    setPulseWidth (motorpin, uint32_t period_us);
-    setDutyPercentage (motorpin, uint32_t percentage);
+//     //motor two
+//     setTimePeriod (motorpin, uint32_t period_us);
+//     setFrequency (motorpin, freq_hz);
+//     setPulseWidth (motorpin, uint32_t period_us);
+//     setDutyPercentage (motorpin, uint32_t percentage);
 
 
     // returning the motor to 1500hz
