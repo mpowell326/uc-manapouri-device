7d82300472904172ac6d374f4f71a44314b3cee2
Morgan Powell <map146@uclive.ac.nz>
2016-09-16 16:26:26 +1200

    Change to c++ to use libraries

---
+++ src_main/motors.cpp
@@ -0,0 +1,24 @@
+#include "motors.h"
+#include <Wire.h>
+#define freq_hz 50;
+
+// void Motormove (int motorpin1, int motorpin2, int PWMpercentage, int time){
+
+
+//     //motor one
+//     setTimePeriod (motorpin, uint32_t period_us);
+//     setFrequency (motorpin, freq_hz);
+//     setPulseWidth (motorpin, uint32_t period_us);
+//     setDutyPercentage (motorpin, uint32_t percentage);
+
+//     //motor two
+//     setTimePeriod (motorpin, uint32_t period_us);
+//     setFrequency (motorpin, freq_hz);
+//     setPulseWidth (motorpin, uint32_t period_us);
+//     setDutyPercentage (motorpin, uint32_t percentage);
+
+
+    // returning the motor to 1500hz
+
+
+// }
---
+++ src_main/read_sensors.cpp
@@ -0,0 +1,6 @@
+#include <Wire.h>
+
+// void readsensors(int sensorpin){
+//     int sensorvalue  = analogRead(sensorpin)
+//     sensorvalue = sensorvalue*4.9 // voltage divider values to give actual value
+// }
