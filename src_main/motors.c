af63956fb910d7179ee9e06c9213d13c20c97680
mww38 <mww38@uclive.ac.nz>
2016-09-16 14:26:08 +1200

    update

--- src_main/motors.c
+++ src_main/motors.c
@@ -2,23 +2,34 @@
 #include "wiring.h"
 #define freq_hz 50;
 
-void Motormove (int motorpin1, int motorpin2, int PWMpercentage, int time){
 
+//initialising stuff
+    //motor one
+    setTimePeriod (motorpin, uint32_t period_us);
+    setFrequency (motorpin, freq_hz);
 
     //motor one
     setTimePeriod (motorpin, uint32_t period_us);
     setFrequency (motorpin, freq_hz);
+void Motormove (int motorpin1, int motorpin2, int PWMpercentage, int time){
+
+
+    //motor one
     setPulseWidth (motorpin, uint32_t period_us);
-    setDutyPercentage (motorpin, uint32_t percentage);
 
     //motor two
-    setTimePeriod (motorpin, uint32_t period_us);
-    setFrequency (motorpin, freq_hz);
     setPulseWidth (motorpin, uint32_t period_us);
-    setDutyPercentage (motorpin, uint32_t percentage);
+
 
 
     // returning the motor to 1500hz
+
+    //motor one
+    setPulseWidth (motorpin, uint32_t period_us);
+
+    //motor two
+    setPulseWidth (motorpin, uint32_t period_us);
+
 
 
 }
